1bc9141bf988465bc0a9c2edbaf93fe5c9dbec2e
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-22 12:57:02 +0200

    Initial commit

---
+++ src/test/java/com/eriksommer/webprogsikkerhet/WebProgSikkerhetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eriksommer.webprogsikkerhet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProgSikkerhetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
