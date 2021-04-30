52c7930454b1be1af976c3aec032724b8e91bb80
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-30 12:10:23 +0200

    Klient implementasjon etter andre time

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,6 +1,34 @@
 import validering from "./validering.js"
 
 $(() => {
+    $("#loggInn").click(() => {
+        if (validering.loginn()){
+            const url = "/loggInn?brukernavn="+$("#brukernavn").val()
+                +"&passord="+$("#passord").val();
+
+            $.get(url, OK => {
+                if (OK){
+                    window.location.href = "/liste.html";
+                }else {
+                    $("#feil").html("Feil i brukernavn eller passord");
+                }
+            })
+        }
+    })
+
+    $("#registrer").click(() => {
+        window.location.href = "/nybruker.html";
+    });
+
+    $("#krypterAllePassord").click(() => {
+        $.get("/krypterAllePassord", OK => {
+            if (OK){
+                $("#feil").html("Kryptering fullført!")
+            } else {
+                $("#feil").html("Kryptering feilet!")
+            }
+        })
+    })
 
     $("#brukernavn").on("change", () => validering.brukernavn());
     $("#passord").on("change", () => validering.passord());
--- src/main/resources/static/nybruker.js
+++ src/main/resources/static/nybruker.js
@@ -1,5 +1,27 @@
 import validering from "./validering.js"
 
 $(() => {
+    $("#registrer").click(() => {
+        const bruker = {
+            brukernavn : $("#brukernavn").val(),
+            passord : $("#passord").val()
+        }
 
+        if (validering.loginn()){
+            $.post("/nybruker", bruker, () => {
+                const url = "/loggInn?brukernavn="+bruker.brukernavn
+                    +"&passord="+bruker.passord
+                $.get(url, OK => {
+                    if (OK){
+                        window.location.href = "liste.html"
+                    }else {
+                        $("#feil").html("Feil i brukernavn eller passord");
+                    }
+                })
+            }).fail(jqXHR => {
+                const json = $.parseJSON(jqXHR.responseText);
+                $("#feil").html(json.message)
+            })
+        }
+    })
 });
